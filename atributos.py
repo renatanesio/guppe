a5cca6cdf05c4e4599748bd7a67aa12d7ce37a52
Renata Nésio <renata.nesio@mail.ru>
2020-12-18 10:05:15 -0300

    Atributos POO

--- atributos.py
+++ atributos.py
@@ -6,6 +6,7 @@ conseguimos representar computacionalmente os estados de um objeto.
 
 Em Python, dividimos os atributos em 3 grupos:
     - Atributos de Instância
+        - Podem ser públicos ou privados
     - Atributos de Classe
     - Atributos Dinâmicos
 
@@ -15,6 +16,26 @@ OBS: Método construtor: é um método especial utilizado para a construção do
 
 
 # Método: funções dentro de uma classe
+
+Em Python, por convenção, todo atributo de uma classe é público.
+Ou seja, pode ser acessado em todo o projeto.
+Caso queiramos demonstrar que determinado atributo deve ser tratado como privado,
+utiliza-se duplo underscore __ no início de seu nome.
+
+Isso é conhecido também como Name Mangling.
+
+# O que significa atributo de instância?
+
+# Significa que, ao criarmos instâncias/objetos de uma classe, todas as instâncias
+# terão estes atributos.
+
+user1 = Acesso('user1@gmail.com', '123456')
+user2 = Acesso('user2@gmail.com', '654321')
+
+user1.mostra_email()
+user2.mostra_email()
+
+
 """
 
 
@@ -38,6 +59,8 @@ class Lampada:
         return self.__ligada
 
 
+# Classes com Atributo de Instância Público
+
 class ContaCorrente:
 
     def __init__(self, numero, limite, saldo):
@@ -47,6 +70,9 @@ class ContaCorrente:
 
 
 class Produto:
+
+    # Atributo de classe
+    imposto = 1.05  # 5% de imposto
 
     def __init__(self, nome, descricao, valor):
         self.nome = nome
@@ -60,3 +86,42 @@ class Usuario:
         self.nome = nome
         self.email = email
         self.senha = senha
+
+
+# Atributos Públicos e Atributos Privados
+
+class Acesso:
+
+    def __init__(self, email, senha):
+        self.email = email
+        self.__senha = senha
+
+    def mostra_senha(self):
+        print(self.__senha)
+
+    def mostra_email(self):
+        print(self.email)
+
+
+# OBS: lembre-se que isso é apenas uma convenção, ou seja, a linguagem Python
+# não impedirá que façamos acesso aos atributos sinalizados como privados fora da classe
+
+# Exemplo
+
+user = Acesso('user@gmail.com', '123456')
+user.mostra_email()
+user.mostra_senha()
+
+# print(user.__senha)  # AttributeError
+
+# print(user._Acesso__senha)  # Temos acesso, mas não deveríamos fazer este acesso (Name mangling)
+
+
+# Atributos de Classe
+# São atributos que são declarados diretamente na classe, ou seja, fora do construtor.
+# Geralmente já inicializamos um valor, e este valor é compartilhado entre todas as
+# instâncias da classe. Isto é, em vez de cada instância da classe ter seus próprios valores,
+# com os atributos de classe todas as instâncias terão o mesmo valor para este atributo.
+
+p1 = Produto('PlayStation 4', 'Console', 2500.00)
+p2 = Produto('PlayStation 5', 'Console', 5090.00)
